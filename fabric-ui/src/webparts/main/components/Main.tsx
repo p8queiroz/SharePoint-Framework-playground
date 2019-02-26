071580b91d3d5aabf043a158a677cf12f5a70df9
Paulo Queiroz <phqs.phqs@gmail.com>
2019-02-26 15:21:41 -0300

    add office ui component

--- fabric-ui/src/webparts/main/components/Main.tsx
+++ fabric-ui/src/webparts/main/components/Main.tsx
@@ -2,24 +2,96 @@ import * as React from 'react';
 import styles from './Main.module.scss';
 import { IMainProps } from './IMainProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { Fabric } from "office-ui-fabric-react";
+import { GroupedList } from 'office-ui-fabric-react/lib/components/GroupedList/index';
+
 
 export default class Main extends React.Component<IMainProps, {}> {
+  
+  private items  = [];
+  private groups = [];
+
+  constructor() {
+    super();
+    
+    this.items = [
+      { key: String(0) },
+      { key: String(1) },
+      { key: String(2) },
+      null, null,
+      { key: String(5) },
+      { key: String(6) },
+      { key: String(7) },
+      null, null, null, null, null, null, null
+    ]; // partially filled items list
+    
+    this.groups = [{
+      key: '1',
+      name: 'Group1',
+      startIndex: 0,
+      count: 5
+    }, {
+      key: '2',
+      name: 'Group2',
+      startIndex: 5,
+      count: 10
+    }]
+  }
+
   public render(): React.ReactElement<IMainProps> {
+    let { items, groups } = this;    
+
+    let groupProps = {
+      getGroupItemLimit: this._getGroupItemLimit.bind(this),
+      showAllProps: {
+        onToggleSummarize: this._onShowAll.bind(this)
+      }
+    };
+
     return (
-      <div className={ styles.main }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Fabric >
+        <GroupedList
+         items={ items }
+         groups={ groups }
+         groupProps={ groupProps }
+         onRenderCell={ this._onRenderCell }
+        />
+      </Fabric>
     );
   }
+
+  private _onRenderCell(nestingDepth?: number,
+    item?: any,
+    index?: number): React.ReactNode {
+    return item && (
+      <div>{ item.key }</div>
+    );
+  }
+  
+  private _getGroupItemLimit(group): number {
+    return group.isShowingAll ? Infinity: 3;
+  }
+
+  private _onShowAll(group) {
+    group.isShowingAll = true;
+    this._fetchAllItems(group).then((newItems) => {;
+      this.items = [ ...newItems ];
+      this.groups = [ ...this.groups ];
+      this.forceUpdate();
+    });
+  }
+ 
+  // mimics fetching more items for this group from server
+  private _fetchAllItems(group): Promise<any> {
+    return new Promise<any>((resolve) => {
+      let items = this.items;
+      let start = group.startIndex;
+      let end = group.startIndex + group.count - 1;
+      for (let i=start; i<=end; i++) {
+        items[i] = { key: String(i) };
+      }
+      setTimeout(() => { resolve(items); }, 1000);
+    });
+  }
 }
+
